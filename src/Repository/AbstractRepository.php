b49ce70217cdbac4c3e3b74925590d49c3c77612
Bára Václav <vaclav.bara@vlmedia.cz>
2023-05-09 07:24:17 +0200

    Abstract Repository: findActiveBy method instead of findDeleted and findActive

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -4,37 +4,28 @@ namespace Blog\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
-class AbstractRepository extends ServiceEntityRepository
+abstract class AbstractRepository extends ServiceEntityRepository
 {
-    public function findActive(array $criteria, ?array $orderBy = null, $limit = null, $offset = null, bool $active = true): array
+
+    public function findActiveBy(array $criteria, ?array $orderBy = null, $limit = null, $offset = null): array
     {
         if (isset($criteria['active'])) {
             unset($criteria['active']);
         }
 
-        $criteria = array_merge($criteria, ['active' => $active]);
-
-        return $this->findBy($criteria, $orderBy, $limit, $offset);
-    }
-
-    public function findDeleted(array $criteria, ?array $orderBy = null, $limit = null, $offset = null): array
-    {
         if (isset($criteria['deleted'])) {
             unset($criteria['deleted']);
         }
 
-        $criteria = array_merge($criteria, ['deleted' => true]);
+        $columns = $this->getClassMetadata()->getColumnNames();
 
-        return $this->findBy($criteria, $orderBy, $limit, $offset);
-    }
-
-    public function findUndeleted(array $criteria, ?array $orderBy = null, $limit = null, $offset = null): array
-    {
-        if (isset($criteria['deleted'])) {
-            unset($criteria['deleted']);
+        if (in_array('active', $columns)) {
+            $criteria = array_merge($criteria, ['active' => true]);
         }
 
-        $criteria = array_merge($criteria, ['deleted' => false]);
+        if (in_array('deleted', $columns)) {
+            $criteria = array_merge($criteria, ['deleted' => false]);
+        }
 
         return $this->findBy($criteria, $orderBy, $limit, $offset);
     }
